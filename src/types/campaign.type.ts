deefbecf0d9951f124311bc31a3bc823afe7f92d
CJ <tiennd@northstudio.vn>
2024-04-27 13:55:00 +0700

    :label: add campaign type

---
+++ src/types/campaign.type.ts
@@ -0,0 +1,49 @@
+export interface Ad {
+	name: string;
+	quantity?: number;
+}
+
+export interface SubCampaign {
+	name: string;
+	status: boolean;
+	ads: Ad[];
+}
+
+export interface CampaignInformation {
+	name: string;
+	describe?: string;
+}
+
+export interface Campaign {
+	information: CampaignInformation;
+	subCampaigns: SubCampaign[];
+}
+
+export interface Error {
+	error: string;
+}
+
+export interface AdError extends Ad {
+	id?: number;
+	nameError?: string;
+	quantityError?: string;
+}
+
+export interface CampaignInformationError extends CampaignInformation, Error {}
+
+export interface SubCampaignError extends SubCampaign, Error {
+	id: number,
+	ads: AdError[]
+}
+
+export interface CampaignError  {
+	information: CampaignInformationError;
+	subCampaigns: SubCampaignError[];
+	validation: boolean;
+	hasError: boolean;
+}
+
+
+
+
+
